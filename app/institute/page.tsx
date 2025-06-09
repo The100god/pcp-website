08595a304d4c3ec41e9a6af2c3101106a51c6838
saurabh goyal <89391860+The100god@users.noreply.github.com>
2025-06-09 19:56:58 +0530

    updated

--- app/institute/page.tsx
+++ app/institute/page.tsx
@@ -3,8 +3,8 @@ export default function Institute() {
     <div className="flex flex-col justify-center items-center w-full mt-36 p-6">
       <div className="flex flex-col justify-center items-center w-[80%] p-5 pb-7 bg-white shadow-md">
 
-      <h1 className="text-5xl font-bold mb-4 ">About Our Instituten</h1>
-      <p className="flex flex-wrap text-wrap text-xl">&quot;Established in 2025, our institute has been dedicated to nurturing young minds with holistic education.&quot;</p>
+      <h1 className="text-4xl md:text-5xl font-bold mb-4 ">About Our Institute</h1>
+      <p className="flex flex-wrap w-full text-wrap text-lg md:text-xl">&quot;Established in 2025, our institute has been dedicated to nurturing young minds with holistic education.&quot;</p>
       </div>
     </div>
     
--- app/vision/page.tsx
+++ app/vision/page.tsx
@@ -3,8 +3,8 @@ export default function Vision() {
     <div className="flex flex-col justify-center items-center w-full mt-36 p-6">
       <div className="flex flex-col justify-center items-center w-[80%] p-5 pb-7 bg-white shadow-md">
 
-      <h1 className="text-5xl font-bold mb-4 ">Our Vision</h1>
-      <p className="flex flex-wrap text-wrap text-xl">&quot;Our school aims to provide quality education for all children, fostering creativity and critical thinking.&quot;</p>
+      <h1 className="text-4xl md:text-5xl font-bold mb-4 ">Our Vision</h1>
+      <p className="flex flex-wrap text-center text-wrap text-lg md:text-xl w-full">&quot;Our school aims to provide quality education for all children, fostering creativity and critical thinking.&quot;</p>
       </div>
     </div>
   );
